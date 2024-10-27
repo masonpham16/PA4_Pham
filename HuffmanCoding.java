9af8c785f7da7849d103b31795257f2188a4c5be
Mason Pham <59492883+masonpham16@users.noreply.github.com>
2024-10-26 23:53:01 -0400

    Create HuffmanCoding.java

---
+++ HuffmanCoding.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class HuffmanCoding{
+
+    public Hashtable<Character, Integer> getChararacterFrequency(String aStr){
+        //todo
+
+        return null; //replace it with yours
+    }
+    public Hashtable<Character, Integer> getChararacterFrequency(File aFile){
+        //todo
+
+        return null; //replace it with yours
+    }
+    public HuffmanTreeNode buildHuffmanTree(Hashtable<Character, Integer> ht){
+        //todo
+
+        return null; //replace it with yours
+    }
+
+    public Hashtable<Character, String> buildCodeTable(HuffmanTreeNode huffmanTree){
+        //todo
+
+        return null; //replace it with yours
+    }
+
+
+    public String encoding(String aStr, Hashtable<Character, String> codeTable){
+        //todo
+
+        return null; //replace it with yours
+    }
+    public String decode(String binaryStr, HuffmanTreeNode huffmanTree){
+        //todo-optional
+
+        return null; //replace it with yours
+    }
+
+}
